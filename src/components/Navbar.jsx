53dd177578b5afba8db234af442e68c9fb705902
Sayanjit Mukherjee <silverfangthegem@gmail.com>
2025-02-22 10:17:19 +0530

    changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+//import React from 'react'
+import '../index.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="flex justify-items-center align-middle bg-gray-200 p-4">
+            <div>
+                Navbar-left
+           </div>
+           <div>
+                Navbar-right
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
